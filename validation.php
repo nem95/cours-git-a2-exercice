e26b89bfd3783852573d10f4ba5738498732374a
nem95 <naiim.chatti@gmail.com>
2015-10-05 21:16:37 +0200

    modification du fichier

--- validation.php
+++ validation.php
@@ -6,9 +6,19 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
-
-}else{ 
+	if (isUsernameAvailable($db, $_POST['username']) === true) {
+		if (isEmailAvailable($db, $_POST['email']) === true) {
+			 userRegistration($db,  $_POST['username'],  $_POST['email'],  $_POST['password']);
+		}
+		else{
+			echo "l'email n'est pas disponible";
+		}
+	}
+	else{
+		echo 'l\'username est deja n\'est pas disponible';
+	}
+}
+else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
 	header('Location: register.php');
 }
